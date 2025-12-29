582d4c47f74bfad660074bc2e79514453bdf7900
tulikabans1 <92115305+tulikabans1@users.noreply.github.com>
2025-12-29 04:44:02 +0000

    changed server for my expense tracker

--- server.js
+++ server.js
@@ -1747,6 +1747,7 @@ app.put('/my/uploads/:id/member', authMiddleware, async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
